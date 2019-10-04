c4c90ef6a34d226734f6c8e5dd0847d03bb8b877
chunza2542 <chunza2542@hotmail.com>
2019-10-04 15:42:36 +0700

    Solve SimplifyingFractions

--- solutions/10814-fragtion.cpp
+++ solutions/10814-fragtion.cpp
@@ -2,9 +2,30 @@
 
 using namespace std;
 
+typedef unsigned long long chun;
+
+chun GCD(u_int p, u_int q) {
+    if (p < q) {
+        return GCD(q, p);
+    }
+    if (q == 0) {
+        return p;
+    }
+    return GCD(q, p % q);
+}
+
 int main() {
     int T;
     cin >> T;
+
+    for (int t = 0; t < T; ++t) {
+        chun p, q;
+        char c;
+        cin >> p >> c >> q;
+
+        chun d = GCD(p, q);
+        printf("%llu / %llu\n", p / d, q / d);
+    }
 
     return 0;
 }
