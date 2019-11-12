0128d0da67dbcb6cad431889e50a5217d991a843
chunza2542 <chunza2542@hotmail.com>
2019-11-12 15:17:39 +0700

    Update template

--- template.cpp
+++ template.cpp
@@ -22,6 +22,7 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
+typedef vector<bool> vb;
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define pb push_back
 #define mp make_pair
