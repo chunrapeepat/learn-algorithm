ba49d85f76087c5c13353a93bb30f7de94922094
chunza2542 <chunza2542@hotmail.com>
2019-11-29 22:00:30 +0700

    Solve A - Sweet Problem

---
+++ codeforces/sweet-problem.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        vector<int> A;
+        int r, g, b; cin >> r >> g >> b;
+        A.push_back(r);
+        A.push_back(g);
+        A.push_back(b);
+        sort(A.begin(), A.end());
+        reverse(A.begin(), A.end());
+
+        int minV = A[2];
+        int diff = A[0] - A[1];
+        A[0] -= min(diff, A[2]);
+        A[2] = max(0, A[2] - diff);
+
+//        if (A[2] == 0) {
+//            cout << min(A[0], A[1]) + minV << endl;
+//        } else {
+//        }
+        cout << min(A[0]-A[2]/2-(A[2]%2), A[1]-A[2]/2) + minV << endl;
+    }
+    return 0;
+}
