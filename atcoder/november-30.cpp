63008cb090cdbc3094c072c62ec4bafed08947c3
chunza2542 <chunza2542@hotmail.com>
2019-12-01 19:04:23 +0700

    Solve A - November 30

---
+++ atcoder/november-30.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int M, D, M2, D2; cin >> M >> D >> M2 >> D2;
+    if (M == M2) {
+        cout << 0 << endl;
+    } else {
+        cout << 1 << endl;
+    }
+    return 0;
+}
