f7c67c6b2648d354ab9498152263d9aa9d62bf13
chunza2542 <chunza2542@hotmail.com>
2019-12-12 23:44:47 +0700

    Solve Remove One Element

--- codeforces/remove-one-element.cpp
+++ codeforces/remove-one-element.cpp
@@ -46,6 +46,7 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> A[i];
     }
+    int withoutRemove = INT_MIN;
     vi inc(n); inc[0] = 1;
     for (int i = 1; i < n; ++i) {
         if (A[i] > A[i-1]) {
@@ -53,29 +54,81 @@ int main() {
         } else {
             inc[i] = 1;
         }
-    }
-    vi dp(n); dp[0] = 1;
-    int v_max = 1;
-    for (int i = 1; i < n; ++i) {
-        if (A[i] > A[i-1]) {
-            dp[i] = dp[i-1] + 1;
-        } else {
-            if (i-2 >= 0 && A[i-2] < A[i]) {
-                dp[i] = inc[i-2] + 1;
-            } else {
-                dp[i] = 1;
-            }
+        if (inc[i] > withoutRemove) {
+            withoutRemove = inc[i];
         }
-        if (dp[i] > v_max) v_max = dp[i];
     }
-    if (v_max == 1) {
-        cout << 0 << endl;
-    } else {
-        cout << v_max << endl;
+    vi dec(n); dec[n-1] = 1;
+    for (int i = n-2; i >= 0; --i) {
+        if (A[i] < A[i+1]) {
+            dec[i] = dec[i+1] + 1;
+        } else {
+            dec[i] = 1;
+        }
     }
+    int v_max = -1;
+    for (int i = 0; i < n; ++i) {
+        int res;
+        int prev = (i-1 >= 0) ? inc[i-1] : 0;
+        int next = (i+1 < n) ? dec[i+1] : 0;
+        if (A[i-1] < A[i+1]) {
+            res = prev + next;
+        } else {
+            res = max(prev, next);
+        }
+        if (res > v_max) v_max = res;
+    }
+    cout << max(withoutRemove, v_max) << endl;
+//    cout << max(withoutRemove, v_max) << endl;
+//    cout << "Inc: ";
 //    for (int x = 0; x < n; ++x) {
-//        cout << dp[x] << " ";
+//        cout << inc[x] << " ";
+//    }
+//    cout << endl;
+//    cout << "Dec: ";
+//    for (int x = 0; x < n; ++x) {
+//        cout << dec[x] << " ";
 //    }
 //    cout << endl;
     return 0;
 }
+
+//int main() {
+//    int n; cin >> n;
+//    vi A(n);
+//    for (int i = 0; i < n; ++i) {
+//        cin >> A[i];
+//    }
+//    vi inc(n); inc[0] = 1;
+//    for (int i = 1; i < n; ++i) {
+//        if (A[i] > A[i-1]) {
+//            inc[i] = inc[i-1] + 1;
+//        } else {
+//            inc[i] = 1;
+//        }
+//    }
+//    vi dp(n); dp[0] = 1;
+//    int v_max = 1;
+//    for (int i = 1; i < n; ++i) {
+//        if (A[i] > A[i-1]) {
+//            dp[i] = dp[i-1] + 1;
+//        } else {
+//            if (i-2 >= 0 && A[i-2] < A[i]) {
+//                dp[i] = inc[i-2] + 1;
+//            } else {
+//                dp[i] = 1;
+//            }
+//        }
+//        if (dp[i] > v_max) v_max = dp[i];
+//    }
+//    if (v_max == 1) {
+//        cout << 0 << endl;
+//    } else {
+//        cout << v_max << endl;
+//    }
+////    for (int x = 0; x < n; ++x) {
+////        cout << dp[x] << " ";
+////    }
+////    cout << endl;
+//    return 0;
+//}
