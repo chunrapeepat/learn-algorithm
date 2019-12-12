ba6f0a1fe61f8da6bb217535a6fecfb161fa4d85
chunza2542 <chunza2542@hotmail.com>
2019-12-12 23:47:25 +0700

    Cleanup code

--- codeforces/remove-one-element.cpp
+++ codeforces/remove-one-element.cpp
@@ -1,5 +1,3 @@
-// Wrong Test
-// 1 2 3 5 3 9 4 5 6
 #include <iomanip>
 #include <iostream>
 #include <sstream>
@@ -28,17 +26,6 @@ typedef vector<bool> vb;
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define pb push_back
 #define mp make_pair
-
-int fact(int n) {
-    int res = 1;
-    for (int i = 2; i <= n; i++)
-        res = res * i;
-    return res;
-}
-
-int nCr(int n, int r) {
-    return fact(n) / (fact(r) * fact(n - r));
-}
 
 int main() {
     int n; cin >> n;
@@ -79,56 +66,5 @@ int main() {
         if (res > v_max) v_max = res;
     }
     cout << max(withoutRemove, v_max) << endl;
-//    cout << max(withoutRemove, v_max) << endl;
-//    cout << "Inc: ";
-//    for (int x = 0; x < n; ++x) {
-//        cout << inc[x] << " ";
-//    }
-//    cout << endl;
-//    cout << "Dec: ";
-//    for (int x = 0; x < n; ++x) {
-//        cout << dec[x] << " ";
-//    }
-//    cout << endl;
     return 0;
 }
-
-//int main() {
-//    int n; cin >> n;
-//    vi A(n);
-//    for (int i = 0; i < n; ++i) {
-//        cin >> A[i];
-//    }
-//    vi inc(n); inc[0] = 1;
-//    for (int i = 1; i < n; ++i) {
-//        if (A[i] > A[i-1]) {
-//            inc[i] = inc[i-1] + 1;
-//        } else {
-//            inc[i] = 1;
-//        }
-//    }
-//    vi dp(n); dp[0] = 1;
-//    int v_max = 1;
-//    for (int i = 1; i < n; ++i) {
-//        if (A[i] > A[i-1]) {
-//            dp[i] = dp[i-1] + 1;
-//        } else {
-//            if (i-2 >= 0 && A[i-2] < A[i]) {
-//                dp[i] = inc[i-2] + 1;
-//            } else {
-//                dp[i] = 1;
-//            }
-//        }
-//        if (dp[i] > v_max) v_max = dp[i];
-//    }
-//    if (v_max == 1) {
-//        cout << 0 << endl;
-//    } else {
-//        cout << v_max << endl;
-//    }
-////    for (int x = 0; x < n; ++x) {
-////        cout << dp[x] << " ";
-////    }
-////    cout << endl;
-//    return 0;
-//}
