a97e31e8fa3a17b57ca8bb11f3c61906927c1a89
chunza2542 <chunza2542@hotmail.com>
2019-12-17 22:33:32 +0700

    Clean Beautiful Numbers

--- codeforces/beautiful-number.cpp
+++ codeforces/beautiful-number.cpp
@@ -1,31 +1,8 @@
-#include <iomanip>
 #include <iostream>
-#include <sstream>
-#include <cstdio>
-#include <cmath>
-#include <climits>
-#include <cstring>
-#include <cctype>
-#include <string>
 #include <vector>
-#include <list>
-#include <set>
-#include <map>
-#include <queue>
-#include <stack>
-#include <algorithm>
-#include <functional>
 
 using namespace std;
-
-typedef long long ll;
-typedef unsigned long long ull;
 typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<bool> vb;
-#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define pb push_back
-#define mp make_pair
 
 int main() {
     int t; cin >> t;
@@ -51,7 +28,6 @@ int main() {
                     l--;
                 }
             }
-//            cout << i << " -> " << l << " " << r << endl;
             if (r - l + 1 == i) {
                 res[i] = 1;
             } else {
