0dfef91e757c00f1fd1a274bca80b428cea11a96
chunza2542 <chunza2542@gmail.com>
2020-04-10 16:33:06 +0700

    Add unsolve Modulo Equality

---
+++ codeforces/modulo-equality.cpp
@@ -0,0 +1,102 @@
+// template-cpp-algor
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+// type
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+// shortcuts
+#define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define pii pair<int, int>
+#define vpii vector<pii>
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define F first
+#define S second
+#define R first
+#define C second
+#define X first
+#define Y second
+#define INIT_RANDOM_SEED srand(time(0));
+// print
+template<typename T, typename... Types>
+void print_with_space(T t, Types... ts) {
+    cout << t << " ";
+}
+template<typename T, typename... Args>
+void DEBUG(T t, Args... args) {
+    cout << "DEBUG: " << t << " "; print_with_space(args...); cout << endl;
+}
+
+int fact(int n) {
+    int res = 1;
+    for (int i = 2; i <= n; i++)
+        res = res * i;
+    return res;
+}
+
+int nCr(int n, int r) {
+    return fact(n) / (fact(r) * fact(n - r));
+}
+
+/* CODE START BELOW THIS POINT!!! */
+int m;
+map<int, int> used;
+
+bool is_equal(vi a, vi& b, int x) {
+    for (int i = 0; i < a.size(); ++i) {
+        a[i] = (a[i] + x) % m;
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n; cin >> n >> m;
+    vi a(n);
+    vi b(n);
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+    for (int i = 0; i < n; ++i)
+        cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    for (int i = 0; i < n; ++i) {
+        int x = (b[i] - a[i]) % m;
+        if (used.find(x) != used.end()) continue;
+        used[x] = 1;
+        if (is_equal(a, b, x)) {
+            cout << x << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
