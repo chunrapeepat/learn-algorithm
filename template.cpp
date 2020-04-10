4cb92ea1d423f005ccb5e57986c22ade62502327
chunza2542 <chunza2542@gmail.com>
2020-04-10 10:03:21 +0700

    Update C++ template

--- template.cpp
+++ template.cpp
@@ -18,14 +18,29 @@
 
 using namespace std;
 
+// type
 typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<bool> vb;
+// shortcuts
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define pb push_back
 #define mp make_pair
+#define F first
+#define S second
+#define X first
+#define Y second
+// print
+template<typename T>
+void _PRINT_WITH_SPACE(T t) {
+    cout << t << " ";
+}
+template<typename T, typename... Args>
+void DEBUG(T t, Args... args) {
+    cout << "DEBUG: " << t << " "; _PRINT_WITH_SPACE(args...); cout << endl;
+}
 
 int fact(int n) {
     int res = 1;
@@ -38,7 +53,8 @@ int nCr(int n, int r) {
     return fact(n) / (fact(r) * fact(n - r));
 }
 
+/* CODE START BELOW THIS POINT!!! */
+
 int main() {
-    // type your code here...
     return 0;
 }
