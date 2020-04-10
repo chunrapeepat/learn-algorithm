0fda4197ea4142d8dab948498675245c4487cda7
chunza2542 <chunza2542@gmail.com>
2020-04-10 14:34:09 +0700

    Update C++ template

--- template.cpp
+++ template.cpp
@@ -24,22 +24,29 @@ typedef unsigned long long ull;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<bool> vb;
+typedef vector<vb> vvb;
 // shortcuts
 #define FAST_IO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define pii pair<int, int>
+#define vpii vector<pii>
 #define pb push_back
+#define pf push_front
 #define mp make_pair
 #define F first
 #define S second
+#define R first
+#define C second
 #define X first
 #define Y second
+#define INIT_RANDOM_SEED srand(time(0));
 // print
-template<typename T>
-void _PRINT_WITH_SPACE(T t) {
+template<typename T, typename... Types>
+void print_with_space(T t, Types... ts) {
     cout << t << " ";
 }
 template<typename T, typename... Args>
 void DEBUG(T t, Args... args) {
-    cout << "DEBUG: " << t << " "; _PRINT_WITH_SPACE(args...); cout << endl;
+    cout << "DEBUG: " << t << " "; print_with_space(args...); cout << endl;
 }
 
 int fact(int n) {
