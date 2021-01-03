a7115fd8a77fc19139b734ede20ec1baf6de29a7
chunza2542 <hello@thechun.dev>
2021-01-03 20:04:19 +0700

    Solve Special Permutation

--- codeforces/special-permutation.cpp
+++ codeforces/special-permutation.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 int main() {
-  cout << "Hello my name is Chun" << endl;
+  int t; cin >> t;
+
+  while (t--) {
+    int n; cin >> n;
+    int m = (n / 2) + 1;
+
+    for (int i = n; i >= 1; i--) {
+      if (n % 2 == 1 && i == m) {
+        continue;
+      }
+      cout << i << ' ';
+    }
+
+    if (n % 2 == 1) {
+      cout << m << ' ';
+    }
+
+    cout << endl;
+  }
+
   return 0;
 }
